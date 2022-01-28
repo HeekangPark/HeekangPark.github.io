5b1efa778729d831b8e94d53f984f08642cbe175
Heekang Park <park.heekang33@gmail.com>
2022-01-28 10:59:27 +0900

    commit from laptop, 2022-01-28 10:59

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const COMMIT_TIME = "2022-01-28 10:58";
+const COMMIT_TIME = "2022-01-28 10:59";
 
 const CACHE_STORAGE_NAME = "Reinventing The Wheel"
 const STATIC_CACHE_STORAGE_NAME = `${CACHE_STORAGE_NAME} - static`
