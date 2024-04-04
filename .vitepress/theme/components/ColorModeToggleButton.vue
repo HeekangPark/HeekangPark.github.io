c05fb09397f903852ade0e3a8e3b6dac19ed1c8e
Heekang Park <park.heekang33@gmail.com>
2024-04-05 01:18:07 +0900

    v2.0.0-beta.9

--- .vitepress/theme/components/ColorModeToggleButton.vue
+++ .vitepress/theme/components/ColorModeToggleButton.vue
@@ -11,15 +11,17 @@ const onColorModeToggleBtnClicked = () => {
 </script>
 
 <template>
-  <div class="color-mode-toggle-btn" @click="onColorModeToggleBtnClicked">
-    <div class="background light-mode-background" v-if="!isDark">
-      <v-icon name="bi-sun" scale="1.1" title="Light mode" />
+  <ClientOnly>
+    <div class="color-mode-toggle-btn" @click="onColorModeToggleBtnClicked">
+      <div class="background light-mode-background" v-if="!isDark">
+        <v-icon name="bi-sun" scale="1.1" title="Light mode" />
+      </div>
+      <div class="background dark-mode-background" v-if="isDark">
+        <v-icon name="bi-moon" scale="1.1" title="Dark mode" />
+      </div>
+      <div class="ball" :class="{ 'light': !isDark, 'dark': isDark }"></div>
     </div>
-    <div class="background dark-mode-background" v-if="isDark">
-      <v-icon name="bi-moon" scale="1.1" title="Dark mode" />
-    </div>
-    <div class="ball" :class="{'light': !isDark, 'dark': isDark}"></div>
-  </div>
+  </ClientOnly>
 </template>
 
 <style scoped lang="scss">
@@ -31,12 +33,12 @@ const onColorModeToggleBtnClicked = () => {
   $border: 0px;
   $background-width: calc($ball-size * 2 + $padding * 2 + $padding + $border * 2);
   $background-height: calc($ball-size + $padding * 2 + $border * 2);
-  
+
   --color-mode-toggle-btn-background: var(--site-panel-background);
   --color-mode-toggle-btn-ball-light: var(--site-panel-background);
   --color-mode-toggle-btn-ball-dark: var(--site-panel-background);
 
-  
+
   display: inline-block;
   cursor: pointer;
   position: relative;
@@ -53,7 +55,7 @@ const onColorModeToggleBtnClicked = () => {
     background: var(--color-mode-toggle-btn-background);
     border: $border solid transparent;
     @include shadow_inset_small;
-    
+
     &.light-mode-background {
       justify-content: flex-end;
     }
